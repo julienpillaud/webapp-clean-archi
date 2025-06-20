a59afb9ad94e9a35e288e291bdf695298517e1c9
Julien Pillaud <julien.pillaud@gmail.com>
2025-06-20 19:52:57 +0200

    feat: add CLI commands for post management

--- app/cli/users.py
+++ app/cli/users.py
@@ -1,37 +1,28 @@
-import uuid
 from typing import Annotated
 
 import typer
-from pydantic import ValidationError
-from rich import print
+from pydantic import PositiveInt
 
-from app.cli.utils import exception_handler, print_result
-from app.domain.entities import EntityId, Pagination
-from app.domain.users.entities import UserCreate
+from app.cli.utils import (
+    get_domain_method,
+    make_model_parser,
+    parse_entity_id,
+    print_result,
+)
+from app.domain.entities import DEFAULT_PAGINATION_LIMIT, EntityId, Pagination
+from app.domain.users.entities import UserCreate, UserUpdate
 
 app = typer.Typer()
 
 
-def parse_user_create(value: str) -> UserCreate:
-    try:
-        return UserCreate.model_validate_json(value)
-    except ValidationError as error:
-        print(f"[red]Error parsing UserCreate data: {error}[/red]")
-        raise typer.Exit(code=1) from error
-
-
-def parse_entity_id(value: str) -> EntityId:
-    try:
-        return uuid.UUID(value)
-    except ValueError:
-        return value
-
-
 @app.command()
-def get_all(ctx: typer.Context) -> None:
-    domain = ctx.obj["domain"]
-    get_users = exception_handler(domain.get_users)
-    users = get_users(Pagination())
+def get_all(
+    ctx: typer.Context,
+    page: Annotated[PositiveInt, typer.Argument()] = 1,
+    limit: Annotated[PositiveInt, typer.Argument()] = DEFAULT_PAGINATION_LIMIT,
+) -> None:
+    get_users = get_domain_method(ctx=ctx, method_name="get_users")
+    users = get_users(pagination=Pagination(page=page, limit=limit))
     print_result(result=users, title="Users")
 
 
@@ -40,8 +31,7 @@ def get(
     ctx: typer.Context,
     user_id: Annotated[EntityId, typer.Argument(parser=parse_entity_id)],
 ) -> None:
-    domain = ctx.obj["domain"]
-    get_user = exception_handler(domain.get_user)
+    get_user = get_domain_method(ctx=ctx, method_name="get_user")
     user = get_user(user_id=user_id)
     print_result(result=user, title="User")
 
@@ -49,9 +39,35 @@ def get(
 @app.command()
 def create(
     ctx: typer.Context,
-    data: Annotated[UserCreate, typer.Argument(parser=parse_user_create)],
+    data: Annotated[
+        UserCreate,
+        typer.Argument(parser=make_model_parser(UserCreate)),
+    ],
 ) -> None:
-    domain = ctx.obj["domain"]
-    create_user = exception_handler(domain.create_user)
+    create_user = get_domain_method(ctx=ctx, method_name="create_user")
     user = create_user(data=data)
     print_result(result=user, title="User created", border_style="green")
+
+
+@app.command()
+def update(
+    ctx: typer.Context,
+    user_id: Annotated[EntityId, typer.Argument(parser=parse_entity_id)],
+    data: Annotated[
+        UserUpdate,
+        typer.Argument(parser=make_model_parser(UserUpdate)),
+    ],
+) -> None:
+    update_user = get_domain_method(ctx=ctx, method_name="update_user")
+    user = update_user(user_id=user_id, data=data)
+    print_result(result=user, title="User updated", border_style="green")
+
+
+@app.command()
+def delete(
+    ctx: typer.Context,
+    user_id: Annotated[EntityId, typer.Argument(parser=parse_entity_id)],
+) -> None:
+    delete_user = get_domain_method(ctx=ctx, method_name="delete_user")
+    delete_user(user_id=user_id)
+    print_result(result=None, title="User deleted", border_style="green")
--- app/cli/utils.py
+++ app/cli/utils.py
@@ -1,15 +1,21 @@
+import re
+import uuid
 from collections.abc import Callable
 from typing import Any, ParamSpec, TypeVar
 
 import typer
+from pydantic import BaseModel, ValidationError
 from rich import print
 from rich.panel import Panel
 from rich.pretty import Pretty
 
+from app.domain.entities import EntityId
 from app.domain.exceptions import AlreadyExistsError, NotFoundError
 
 P = ParamSpec("P")
 R = TypeVar("R")
+
+object_id_pattern = re.compile(r"^[a-fA-F0-9]{24}$")
 
 
 def exception_handler(function: Callable[P, R]) -> Callable[P, R]:
@@ -24,6 +30,33 @@ def exception_handler(function: Callable[P, R]) -> Callable[P, R]:
             raise typer.Exit(code=1) from error
 
     return decorator
+
+
+def get_domain_method(ctx: typer.Context, method_name: str) -> Callable[..., Any]:
+    domain = ctx.obj["domain"]
+    return exception_handler(getattr(domain, method_name))
+
+
+def make_model_parser(model: type[BaseModel]) -> Callable[[str], BaseModel]:
+    def parser(value: str) -> BaseModel:
+        try:
+            return model.model_validate_json(value)
+        except ValidationError as error:
+            raise typer.BadParameter(
+                f"Can't parse data to '{model.__name__}'"
+            ) from error
+
+    return parser
+
+
+def parse_entity_id(value: str) -> EntityId:
+    try:
+        uuid.UUID(value)
+        return value
+    except ValueError:
+        if object_id_pattern.match(value):
+            return value
+    raise typer.BadParameter("Must be UUID or ObjectId.")
 
 
 def print_result(result: Any, title: str, border_style: str = "none") -> None:
