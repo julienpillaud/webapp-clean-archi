bd925898c94251f2bdf3ced8e3b3f39cb8645873
Julien Pillaud <julien.pillaud@gmail.com>
2026-02-06 11:49:48 +0100

    feat: update domain handlers

--- app/domain/domain.py
+++ app/domain/domain.py
@@ -1,4 +1,4 @@
-from cleanstack.domain import BaseDomain, CommandHandler
+from cleanstack.domain import BaseDomain, CommandHandler, QueryHandler
 
 from app.domain.context import ContextProtocol
 from app.domain.dev.commands import (
@@ -22,19 +22,19 @@ from app.domain.users.commands import (
 
 
 class Domain(BaseDomain[ContextProtocol]):
-    get_posts = CommandHandler(get_posts_command)
-    get_post = CommandHandler(get_post_command)
+    get_posts = QueryHandler(get_posts_command)
+    get_post = QueryHandler(get_post_command)
     create_post = CommandHandler(create_post_command)
     update_post = CommandHandler(update_post_command)
     delete_post = CommandHandler(delete_post_command)
 
-    get_user_by_provider_id = CommandHandler(get_user_by_provider_id_command)
-    get_users = CommandHandler(get_users_command)
+    get_user_by_provider_id = QueryHandler(get_user_by_provider_id_command)
+    get_users = QueryHandler(get_users_command)
 
-    get_dummies = CommandHandler(get_dummies_command)
-    get_dummies_cached = CommandHandler(get_dummies_cached_command)
+    get_dummies = QueryHandler(get_dummies_command)
+    get_dummies_cached = QueryHandler(get_dummies_cached_command)
 
-    benchmark = CommandHandler(benchmark_command)
-    custom_error = CommandHandler(custom_error_command)
-    unexpected_error = CommandHandler(unexpected_error_command)
-    unexpected_domain_error = CommandHandler(unexpected_domain_error_command)
+    benchmark = QueryHandler(benchmark_command)
+    custom_error = QueryHandler(custom_error_command)
+    unexpected_error = QueryHandler(unexpected_error_command)
+    unexpected_domain_error = QueryHandler(unexpected_domain_error_command)
